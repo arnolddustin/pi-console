b2ab75b4a94d5bb19b07766a7017a8e2e869d657
Dustin Arnold <dustin.arnold@compassphs.com>
2017-09-29 14:19:27 -0500

    added solution, license, and unit test project; fixed breaking changes from pi-dotnetcore changes

--- pi-console/Program.cs
+++ pi-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using pi_dotnetcore;
 
 namespace pi_console
 {
@@ -7,28 +8,81 @@ namespace pi_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Connecting to GPIO...");
-
-            var ctl = new GpioController();
-            var delay = 250;
-
-            for (int i = 0; i < 10; i++)
+            if (args == null || args.Length < 2)
             {
-                ctl.SetPin(12, true);
-                Task.Delay(delay).Wait();
-                ctl.SetPin(16, true);
-                Task.Delay(delay).Wait();
-                ctl.SetPin(18, true);
-                Task.Delay(delay).Wait();
-                ctl.SetPin(12, false);
-                Task.Delay(delay).Wait();
-                ctl.SetPin(16, false);
-                Task.Delay(delay).Wait();
-                ctl.SetPin(18, false);
-                Task.Delay(delay).Wait();
+                Console.WriteLine("missing required parameter.\n");
+                Console.WriteLine("usage: pi-console status|on|off <pin>");
+                Console.WriteLine("\n  ex: get status of pin 12:");
+                Console.WriteLine("  pi-console status 12");
+                Console.WriteLine("\n  ex: turn pin 16 on");
+                Console.WriteLine("  pi-console on 16");
+                Console.WriteLine("\n  ex: turn pin 18 off");
+                Console.WriteLine("  pi-console off 18");
+                return;
+            }
+
+            try
+            {
+                switch (args[0].ToLower())
+                {
+                    case "status":
+                        Status(args);
+                        return;
+
+                    case "on":
+                        On(args);
+                        return;
+
+                    case "off":
+                        Off(args);
+                        return;
+
+                    default:
+                        throw new ArgumentOutOfRangeException("command", args[0], "unknown command.");
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
             }
 
             Console.WriteLine("Done.");
+        }
+
+        static void Status(string[] args)
+        {
+            int number;
+            if (!int.TryParse(args[1], out number))
+            {
+                throw new ArgumentOutOfRangeException("number", args[1], "argument 2 must be a number");
+            }
+
+            Console.WriteLine(new Gpio().Get(number).ToString());
+        }
+
+        static void On(string[] args)
+        {
+            int number;
+            if (!int.TryParse(args[1], out number))
+            {
+                throw new ArgumentOutOfRangeException("number", args[1], "argument 2 must be a number");
+            }
+
+            new Gpio().Set(number, GpioDirection.Out, true);
+            Console.WriteLine(string.Format("Pin {0} turned on", number));
+        }
+
+        static void Off(string[] args)
+        {
+            int number;
+            if (!int.TryParse(args[1], out number))
+            {
+                throw new ArgumentOutOfRangeException("number", args[1], "argument 2 must be a number");
+            }
+
+            new Gpio().Set(number, GpioDirection.Out, false);
+            Console.WriteLine(string.Format("Pin {0} turned off", number));
         }
     }
 }
